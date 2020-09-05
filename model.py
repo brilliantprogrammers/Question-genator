4c6e242f0b4612b829321eb3c44791e4ce02a907
Brillianttyagi <deepanshubhai84@gmail.com>
2020-09-05 21:25:16 +0530

    update file

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 from gensim.parsing.preprocessing import remove_stopwords
-
+from removewords import remove_extrawords
 
 string = str(input("Enter your notes::::: "))
+string = string.lower()
 filtered_sentence = remove_stopwords(string)
-with open('ok.txt','r') as fh:
+filtered_sentence = remove_extrawords(filtered_sentence)
+with open('ok2.txt','r') as fh:
      all_lines = fh.readlines()
-print(all_lines)
+
 word_list = list(filtered_sentence.split())
 for i in word_list:
     for j in all_lines:
