bac11fd51dd386dc52bc8834f2f6805e153c573d
Brillianttyagi <deepanshubhai84@gmail.com>
2020-09-06 12:55:40 +0530

    update code

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 from gensim.parsing.preprocessing import remove_stopwords
 from removewords import remove_extrawords
+from questionlist import unique_question
 
 string = str(input("Enter your notes::::: "))
 string = string.lower()
 filtered_sentence = remove_stopwords(string)
 filtered_sentence = remove_extrawords(filtered_sentence)
+question_list = []
 with open('ok2.txt','r') as fh:
      all_lines = fh.readlines()
 
@@ -12,4 +14,8 @@ word_list = list(filtered_sentence.split())
 for i in word_list:
     for j in all_lines:
         if i in j:
-            print(i,j)
+            question_list.append(j)
+
+final_question = unique_question(question_list)
+for i in final_question:
+    print(i)
--- removewords.py
+++ removewords.py
@@ -2,7 +2,6 @@ def remove_extrawords(query):
     with open('ok.txt','r') as fh:
         all_lines = fh.read()
     stopwords = list(all_lines.split(' '))
-    print(stopwords)
     querywords = query.split()
 
     resultwords  = [word for word in querywords if word.lower() not in stopwords]
